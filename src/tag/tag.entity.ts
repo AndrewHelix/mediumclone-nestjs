f6aeddc240ac10b9a659f90c39ea0ce8a6a5e0d5
AndreiShulha <andreishulha@coherentsolutions.com>
2023-02-27 21:12:59 +0300

    db connection

---
+++ src/tag/tag.entity.ts
@@ -0,0 +1,10 @@
+import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
+
+@Entity({ name: 'tags' })
+export class TagEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+}
